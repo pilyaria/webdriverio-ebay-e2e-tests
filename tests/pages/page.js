8a33ccff926cce7c29cdc597b21582db7d0c94e4
Irina <irina_dubna@mail.ru>
2026-08-13 21:10:08 +0300

    Improve shipping dialog handling

--- tests/pages/page.js
+++ tests/pages/page.js
@@ -1,6 +1,16 @@
 export default class Page {
   async open(path) {
     await browser.url(path);
+
+    await browser.waitUntil(
+      async () =>
+        (await browser.execute(() => document.readyState)) === "complete",
+      {
+        timeout: 10000,
+        timeoutMsg: "Page did not finish loading",
+      },
+    );
+
     await this.dismissShippingDialog();
   }
 
@@ -8,7 +18,7 @@ export default class Page {
     const dialog = $(".address-dialog__lightbox[aria-hidden='false']");
 
     const isDisplayed = await dialog
-      .waitForDisplayed({ timeout: 5000 })
+      .waitForDisplayed({ timeout: 7000 })
       .then(() => true)
       .catch(() => false);
 
@@ -17,8 +27,13 @@ export default class Page {
     }
 
     const closeButton = dialog.$(".lightbox-dialog__close");
+
     await closeButton.waitForClickable({ timeout: 5000 });
     await closeButton.click();
-    await dialog.waitForDisplayed({ reverse: true, timeout: 5000 });
+
+    await dialog.waitForDisplayed({
+      reverse: true,
+      timeout: 5000,
+    });
   }
 }
